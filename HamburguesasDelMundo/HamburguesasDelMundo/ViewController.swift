227baacbdea41b300277cfb554465dcd368bd27e
fmoriana <fmoriana@gmail.com>
2016-05-09 11:33:46 +0200

    Add files via upload

--- HamburguesasDelMundo/HamburguesasDelMundo/ViewController.swift
+++ HamburguesasDelMundo/HamburguesasDelMundo/ViewController.swift
@@ -20,22 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    class ColeccionDePaises{
-        let paises:[String] = ["España", "Francia", "Alemania", "Italia", "UK", "Rusia","USA","Canadá","México", "Argentina","Chile","Brasil","India","Australia","China","Japón","Grecia", "Ucrania","Marruecos","Sudáfrica"]
-        func obtenPais( )->String {
-            let numeroAleatorio : Int = Int(arc4random() % UInt32(paises.count) )
-            return paises[numeroAleatorio]
-        }
-    }
-    
-    class ColeccionDeHamburguesas{
-        let hamburguesas:[String] = ["Española", "Argentina", "Ternera", "Raza Nostra", "Deportista", "Del Pueblo","Barbacoa","Bacon","Madrid", "Parmigiano","Ranchera","Ibérica","A la antigüa","Roquefort","Cebolla Pochada","Transilvania","Americana", "Del Chef","4 quesos","Trufa"]
-        func obtenHamburguesa( )->String {
-            let numeroAleatorio : Int = Int(arc4random() % UInt32(hamburguesas.count) )
-            return hamburguesas[numeroAleatorio]
-        }
-    }
-    
+       
     let Paises = ColeccionDePaises ()
     let Hamburguesas = ColeccionDeHamburguesas()
     @IBOutlet weak var etiquetaPais: UILabel!
